351fae74dc29aa9beb5a91ee93eee27c98a1f9a9
Ewan Slater <ewan.slater@gmail.com>
2020-05-27 11:46:51 +0100

    reading socket_path

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -1,8 +1,10 @@
 class FnHelper
   getter(url) { ENV["FN_LISTENER"]? }
-
+  getter(socket_path : String) { url.try(&.[5..]) }
 end
 
 f = FnHelper.new
 STDERR.puts "CRYSTAL RUNTIME"
-STDERR.puts "f.url: #{f.url}"
+STDERR.puts "url: #{f.url}"
+STDERR.puts "socket_path: #{f.socket_path}"
+STDERR.puts "f.inspect: #{f.inspect}"
