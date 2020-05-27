fb9892d3846a627d6904899cad64f9a3a635e7a0
Ewan Slater <ewan.slater@gmail.com>
2020-05-27 15:03:34 +0100

    Links socket

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -1,11 +1,25 @@
 require "socket"
+require "file_utils"
 
 class FnHelper
-  getter(url) { ENV["FN_LISTENER"]? }
-  getter(socket_path : String) { url.try(&.[5..]) }
-  getter(private_socket_path : String) { socket_path.try(&. + ".private") }
+  getter(url : String) { ENV.fetch "FN_LISTENER", "unix:/tmp/iofs/lsnr.sock" }
+  getter(socket_path : String) { url[5..] }
+  getter(private_socket_path : String) { socket_path + ".private" }
+  getter? linked : Bool = false
+
   getter(private_socket : UNIXServer) do
-    private_socket_path.try { |psp| UNIXServer.new psp }
+    UNIXServer.new private_socket_path
+  end
+
+  def socket
+    link_socket_file unless linked?
+    private_socket
+  end
+
+  def link_socket_file
+    File.chmod(private_socket_path, 0o666)
+    FileUtils.ln_s(File.basename(private_socket_path), socket_path)
+    @linked = true
   end
 end
 
@@ -15,5 +29,8 @@ STDERR.puts "url: #{f.url}"
 STDERR.puts "socket_path: #{f.socket_path}"
 STDERR.puts "private_socket_path: #{f.private_socket_path}"
 STDERR.puts "private_socket: #{f.private_socket}"
+STDERR.puts "linked: #{f.linked?}"
+STDERR.puts "socket: #{f.socket}"
+STDERR.puts "linked: #{f.linked?}"
 
 STDERR.puts "f.inspect: #{f.inspect}"
