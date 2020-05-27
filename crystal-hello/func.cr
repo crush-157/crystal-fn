832e52302bbc4c2d2e4f62a04979d02665d3a278
Ewan Slater <ewan.slater@gmail.com>
2020-05-27 11:06:48 +0100

    getting listener url

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -1,1 +1,8 @@
-STDERR.puts "HELP, I AM TRAPPED IN A CRYSTAL MAZE!"
+class FnHelper
+  getter(url) { ENV["FN_LISTENER"]? }
+
+end
+
+f = FnHelper.new
+STDERR.puts "CRYSTAL RUNTIME"
+STDERR.puts "f.url: #{f.url}"
