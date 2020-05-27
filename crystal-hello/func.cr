824dac28d88434fa93650bc60140e8622c8c516e
Ewan Slater <ewan.slater@gmail.com>
2020-05-27 12:02:36 +0100

    private_socket_path

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -1,10 +1,12 @@
 class FnHelper
   getter(url) { ENV["FN_LISTENER"]? }
   getter(socket_path : String) { url.try(&.[5..]) }
+  getter(private_socket_path : String) { socket_path.try(&. + ".private") }
 end
 
 f = FnHelper.new
 STDERR.puts "CRYSTAL RUNTIME"
 STDERR.puts "url: #{f.url}"
 STDERR.puts "socket_path: #{f.socket_path}"
+STDERR.puts "private_socket_path: #{f.private_socket_path}"
 STDERR.puts "f.inspect: #{f.inspect}"
