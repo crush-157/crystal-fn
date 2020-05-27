94a9eaa2411d4d4e24c4bfe83a9de36341d621d0
Ewan Slater <ewan.slater@gmail.com>
2020-05-27 12:24:51 +0100

    Opens a UNIX socket

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -1,7 +1,12 @@
+require "socket"
+
 class FnHelper
   getter(url) { ENV["FN_LISTENER"]? }
   getter(socket_path : String) { url.try(&.[5..]) }
   getter(private_socket_path : String) { socket_path.try(&. + ".private") }
+  getter(private_socket : UNIXServer) do
+    private_socket_path.try { |psp| UNIXServer.new psp }
+  end
 end
 
 f = FnHelper.new
@@ -9,4 +14,6 @@ STDERR.puts "CRYSTAL RUNTIME"
 STDERR.puts "url: #{f.url}"
 STDERR.puts "socket_path: #{f.socket_path}"
 STDERR.puts "private_socket_path: #{f.private_socket_path}"
+STDERR.puts "private_socket: #{f.private_socket}"
+
 STDERR.puts "f.inspect: #{f.inspect}"
