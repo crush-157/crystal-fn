d4e0b67a1fe0fafb6fe813f995b46100d54a0f73
Ewan Slater <ewan.slater@gmail.com>
2020-05-29 16:04:07 +0100

    replaced class with module, FnHelper < 20 LOC

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -3,25 +3,20 @@ require "file_utils"
 require "http/server"
 require "json"
 
-class FnHelper
-  getter(socket_path : String) do
+module FnHelper
+  def self.socket_path
     ENV.["FN_LISTENER"].try(&.[5..]) || "unix:/tmp/iofs/lsnr.sock"
   end
 
-  def handle(&block : JSON::Any -> String)
+  def self.handle(&block : JSON::Any -> String)
     server = HTTP::Server.new do |context|
       body = context.request.body.try(&.gets_to_end)
       body = "{}" if body.try(&.empty?) || body.nil?
       context.response.content_type = "application/json"
       body.try { |b| context.response.print block.call JSON.parse b }
     end
-    STDERR.puts "socket_path: #{socket_path}"
     server.bind UNIXServer.new socket_path
     server.listen
-  end
-
-  def self.handle(&block : JSON::Any -> String)
-    FnHelper.new.handle &block
   end
 end
 
