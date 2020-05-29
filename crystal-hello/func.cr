f39156da170156969fea3675f7a6c1034b5a9085
Ewan Slater <ewan.slater@gmail.com>
2020-05-29 15:49:29 +0100

    Dropped private_socket_path and linking to private socket file behaviour

--- crystal-hello/func.cr
+++ crystal-hello/func.cr
@@ -6,21 +6,6 @@ require "json"
 class FnHelper
   getter(url : String) { ENV.fetch "FN_LISTENER", "unix:/tmp/iofs/lsnr.sock" }
   getter(socket_path : String) { url[5..] }
-  getter(private_socket_path : String) { socket_path + ".private" }
-  getter? linked : Bool = false
-
-  getter(private_socket : UNIXServer) do
-    UNIXServer.new private_socket_path
-  end
-
-  getter(linked_socket : UNIXServer) do
-    private_socket.tap do |ps|
-      ps.path.try do |path|
-        File.chmod(path, 0o666)
-        FileUtils.ln_s(File.basename(path), socket_path)
-      end
-    end
-  end
 
   def handle(&block : JSON::Any -> String)
     server = HTTP::Server.new do |context|
@@ -29,7 +14,7 @@ class FnHelper
       context.response.content_type = "application/json"
       body.try { |b| context.response.print block.call JSON.parse b }
     end
-    server.bind linked_socket
+    server.bind UNIXServer.new socket_path
     server.listen
   end
 
